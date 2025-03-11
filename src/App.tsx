bda03070f111e0077cf4b43e1d2be68fdb2712c7
KonstantinMuliarchuk <kotipod@gmail.com>
2025-03-11 13:36:06 +0100

    last changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import {AppNavigator} from './navigation';
+import {store} from './store';
+import {Provider} from 'react-redux';
 
 function App(): React.JSX.Element {
-  return <AppNavigator />;
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 }
 
 export default App;
